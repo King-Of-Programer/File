e22c1b14fd1bd267b6717c1fd2605b18047f30d5
King-Of-Programer <alexdichemix@gmail.com>
2024-03-12 15:11:56 +0200

    init

---
+++ FileApp/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FileApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int _loginCount = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+            object log = null;
+            App.Current.Properties.TryGetValue("loginCount", out log);
+            if (log != null)
+            {
+                _loginCount = (int)log;
+            }
+            loginCountLabel.Text = $"Количество входов: {_loginCount}";
+            _loginCount++;
+            App.Current.Properties["loginCount"] = _loginCount;
+        }
+    }
+}
